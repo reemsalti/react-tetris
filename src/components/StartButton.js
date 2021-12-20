514312e980ad251666061ab9b9389814e21a05b9
Reem Salti <reem@Reems-MacBook-Air.local>
2021-12-20 18:16:19 -0500

    first commit

---
+++ src/components/StartButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledStartButton } from "./styles/StyledStarButton";
+
+const StartButton = ({ callback }) => (
+    <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
+);
+
+export default StartButton;
